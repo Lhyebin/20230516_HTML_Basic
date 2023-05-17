3d558796dde16a7901186a1e87dfc551f5e1c3c9
Lhyebin <133613625+Lhyebin@users.noreply.github.com>
2023-05-17 16:47:48 +0900

    가로배치2 반응형 메인슬라이드
    가로배치2
    반응형 메인슬라이드

---
+++ js/main01.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.mainslide').slick({
+        // arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        // 자동으로 1초마다 다음장으로 넘어가는 방법 - 기준이 3초 / 1초=1000
+        pauseOnHover: false,
+    });
+})
